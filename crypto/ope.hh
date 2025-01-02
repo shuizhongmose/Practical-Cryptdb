2483ceda17ac6b0182d1532d4560f4c5ef2cc715
shuizhongmose <602554102@qq.com>
2025-01-02 16:57:12 +0800

    修正打印错误

--- crypto/ope.hh
+++ crypto/ope.hh
@@ -1,9 +1,7 @@
 #pragma once
 
 #ifdef ONLINE_OPE
-    std::cout << "Use online OPE ..." << std::endl;
     #include "ope-PRZB11.hh"
 #else
-    std::cout << "Use default OPE ..." << std::endl;
     #include "ope-KS88.hh"
 #endif
